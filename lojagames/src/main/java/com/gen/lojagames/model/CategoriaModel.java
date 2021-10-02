64b7a5f347c71123b061441362d2492539e64f76
Everton <everton.oliveira1@icloud.com>
2021-10-02 13:08:57 -0300

    [Feature Everton - Update ProdutoModel/CategoriaModel]

--- lojagames/src/main/java/com/gen/lojagames/model/CategoriaModel.java
+++ lojagames/src/main/java/com/gen/lojagames/model/CategoriaModel.java
@@ -1,19 +1,33 @@
 package com.gen.lojagames.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
 public class CategoriaModel {
-	
+
 	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
-	
+
 	private @NotBlank String nomeCategoria;
+	
+
+	
+	@OneToMany(mappedBy = "categoriaRelacionada",cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"categoriaRelacionada"})
+	
+	private List<ProdutoModel> categoriaProduto = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -29,5 +43,15 @@ public class CategoriaModel {
 
 	public void setNomeCategoria(String nomeCategoria) {
 		this.nomeCategoria = nomeCategoria;
+
 	}
+
+	public List<ProdutoModel> getCategoriaProduto() {
+		return categoriaProduto;
+	}
+
+	public void setCategoriaProduto(List<ProdutoModel> categoriaProduto) {
+		this.categoriaProduto = categoriaProduto;
+	}
+
 }
--- lojagames/src/main/java/com/gen/lojagames/model/ProdutoModel.java
+++ lojagames/src/main/java/com/gen/lojagames/model/ProdutoModel.java
@@ -4,17 +4,52 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_produto")
 public class ProdutoModel {
-	
+
 	@Id
-	public @GeneratedValue(strategy = GenerationType.IDENTITY) Long id; 
-	
+	private @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+
 	@NotBlank
-	public String descricaoTitulo;
+	private String descricaoTitulo;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	@JsonIgnoreProperties({"categoriaProduto"})
+	private CategoriaModel categoriaRelacionada;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricaoTitulo() {
+		return descricaoTitulo;
+	}
+
+	public void setDescricaoTitulo(String descricaoTitulo) {
+		this.descricaoTitulo = descricaoTitulo;
+		
+	}
+
+	public CategoriaModel getCategoriaRelacionada() {
+		return categoriaRelacionada;
+	}
+
+	public void setCategoriaRelacionada(CategoriaModel categoriaRelacionada) {
+		this.categoriaRelacionada = categoriaRelacionada;
+	}
+
 }
