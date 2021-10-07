d2f280920b8129290ef0afc97f937588f1e11510
WillianX47 <willianx47@gmail.com>
2021-10-07 17:11:50 -0300

    [Feature Will - Add dependency | UserLogin | BasicSecurityConfig]

---
+++ lojagames/src/main/java/com/gen/lojagames/security/BasicSecurityConfig.java
@@ -0,0 +1,8 @@
+package com.gen.lojagames.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class BasicSecurityConfig {
+
+}
