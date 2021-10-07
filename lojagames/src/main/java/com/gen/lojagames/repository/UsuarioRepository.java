4cc7d83eae20097e27bc9ef33e94224e1c5c869e
Zoraten <cosmos_blood@outlook.com>
2021-10-07 15:51:55 -0300

    [Feature Guilherme: - Add UsuarioModel & UsuarioRepository | 07.10.21]

---
+++ lojagames/src/main/java/com/gen/lojagames/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.gen.lojagames.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gen.lojagames.model.UsuarioModel;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+	
+	public Optional<UsuarioModel> findByUsuario (String usuario);
+}
