115c82cbf091a976f2ebdab409cbd9bc52a1a4e3
everton.oliveira1@icloud.com <FreitasOliver>
2021-10-14 19:17:55 -0300

    [Feature Everton: - Update Swagger | 14.10.21]

--- lojagames/src/main/java/com/gen/lojagames/configuration/SwaggerConfig.java
+++ lojagames/src/main/java/com/gen/lojagames/configuration/SwaggerConfig.java
@@ -1,5 +1,55 @@
 package com.gen.lojagames.configuration;
 
-public class SwaggerConfig {
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.gen.lojagames.controller")).paths(PathSelectors.any())
+				.build().apiInfo(metadata()).useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, responseMessage()).globalResponses(HttpMethod.POST, responseMessage())
+				.globalResponses(HttpMethod.PUT, responseMessage())
+				.globalResponses(HttpMethod.DELETE, responseMessage());
+	}
+
+	public static ApiInfo metadata() {
+		return new ApiInfoBuilder().title("API - Loja Games").description("Projeto API Spring - Loja Games")
+				.version("1.0.0").license("Apache License Version 2.0")
+				.licenseUrl("https://github.com/WillianX47/Gen-Projeto_lojaGames.git").contact(contact()).build();
+	}
+
+	private static Contact contact() {
+		return new Contact("Grupo Lampe", "https://github.com/WillianX47/Gen-Projeto_lojaGames.git",
+				"grupolampe@gmail.com");
+	}
+
+	private static List<Response> responseMessage() {
+		return new ArrayList<Response>() {
+			private static final long serialVersionUID = 1L;
+			{
+				add(new ResponseBuilder().code("200").description("Sucesso!").build());
+				add(new ResponseBuilder().code("201").description("Criado!").build());
+				add(new ResponseBuilder().code("400").description("Erro na requisição!").build());
+				add(new ResponseBuilder().code("401").description("Não Autorizado!").build());
+				add(new ResponseBuilder().code("403").description("Proibido!").build());
+				add(new ResponseBuilder().code("404").description("Não Encontrado!").build());
+				add(new ResponseBuilder().code("500").description("Erro!").build());
+			}
+		};
+	}
 }
