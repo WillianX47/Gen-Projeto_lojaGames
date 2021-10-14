bc98f00ade0cc78478f17f0b644d67fe723cc5fc
Eduardo <edu3839@hotmail.com>
2021-10-14 16:28:04 -0300

    [Feature Eduardo - Task: Update UserDetailsServiceImpl | 14.10.21]

--- lojagames/src/main/java/com/gen/lojagames/security/UserDetailsServiceImpl.java
+++ lojagames/src/main/java/com/gen/lojagames/security/UserDetailsServiceImpl.java
@@ -1,5 +1,28 @@
 package com.gen.lojagames.security;
 
-public class UserDetailsServiceImpl {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.gen.lojagames.model.UsuarioModel;
+import com.gen.lojagames.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<UsuarioModel> user = repository.findByUsuario(username);
+		user.orElseThrow(() -> new UsernameNotFoundException(username + "Not Found"));
+		return user.map(UserDetailsImpl::new).get();
+
+	}
 
 }
